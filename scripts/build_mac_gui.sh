b60068c2befb24bb7ddd62398396ae3f4fa4c070
Naruto TAKAHASHI <tnaruto@gmail.com>
2018-12-24 14:57:41 +0900

    fix scripts/build_mac_gui.sh

--- scripts/build_mac_gui.sh
+++ scripts/build_mac_gui.sh
@@ -1,13 +1,35 @@
-#!/bin/bash
+#!/bin/bash -x
 pushd $(dirname $0)
 BASEDIR=$(pwd)
-${SRCDIR:="${BASEDIR}/../gui/"}
-${DSTDIR:="${BASEDIR}/../out/"}
+SRCDIR="${BASEDIR}/../gui/"
+DSTDIR="${BASEDIR}/../out/"
 echo "SRCDIR : ${SRCDIR}"
 echo "DSTDIR : ${DSTDIR}"
 
-QT_PATH="/Users/username/Qt5.9.7/5.9.7/clang_64/bin/"
-PATH="${QT_PATH}:${PATH}"
+if [ ! -z ${QT_PATH+x} ]; then
+    # use environment variable
+    QT_PATH=${QT_PATH}
+elif [ -d /usr/local/opt/qt ]; then
+    # use homebrew
+    QT_PATH="/usr/local/opt/qt"
+else 
+    # search latest Qt
+    if [ ! -d ${HOME}/Qt ]; then
+        echo "not install Qt"
+        exit 1
+    fi
+
+    LATEST_VERSION=`/bin/ls ${HOME}/Qt | egrep '[0-9].*\.[0-9].*' | sort -r | head -n 1`
+    if [ -n ${LATEST_VERSION} ]; then
+        QT_PATH=${HOME}/Qt/${LATEST_VERSION}/clang_64
+        echo ${QT_PATH}
+    else
+        # not install QT
+        echo "not found Qt version from ${HOME}/Qt"
+        exit 1
+    fi
+fi
+PATH="${QT_PATH}/bin:${PATH}"
 
 pushd ${SRCDIR}
 

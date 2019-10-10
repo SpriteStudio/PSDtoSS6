41f8693005d48fed52efe52e2a30c484b0ecc2ba
Toshiyuki Kurooka <kurooka@miobox.jp>
2019-10-10 17:36:59 +0900

    create prepare_mac
    create prepare_mac

---
+++ scripts/prepare_mac.sh
@@ -0,0 +1,24 @@
+SCRIPTDIR=`dirname $0`
+SCRIPTDIR=`cd $SCRIPTDIR && pwd -P`
+BASEDIR=${SCRIPTDIR}/..
+BASEDIR=`cd ${BASEDIR} && pwd -P`
+BUILDDIR=${BASEDIR}/
+BUILDDIR=`cd ${BUILDDIR} && pwd -P`
+
+pushd ${BUILDDIR}
+cd ~
+
+git clone https://github.com/Microsoft/vcpkg.git
+./vcpkg/bootstrap-vcpkg.sh
+
+set VCPKG_DEFAULT_TRIPLET=x64-osx
+
+cd vcpkg
+./vcpkg install tinyxml2
+./vcpkg install zlib
+./vcpkg install libpng
+./vcpkg install libjpeg-turbo
+./vcpkg install icu
+popd > /dev/null # 
+
+

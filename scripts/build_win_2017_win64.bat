55b50836c2ebf8f27fe1a62787e8f01a20308f18
Toshiyuki Kurooka <kurooka@miobox.jp>
2019-12-11 12:27:51 +0900

    github actionではVS2017がやっぱりないので無効に
    github actionではVS2017がやっぱりないので無効にした
    代わりにローカルビルド用にバッチファイルを分けた

--- scripts/build_win_2017_win64.bat
+++ scripts/build_win_2017_win64.bat
@@ -4,7 +4,7 @@ set DSTDIR=%BASEDIR%..\out\
 
 pushd %BASEDIR%
 
-call build_win_cui  %1 || exit /b 1
+call build_win_cui_2017_win64 %1 || exit /b 1
 call build_win_gui %1 || exit /b 1
 
 xcopy /y ..\LICENSE %DSTDIR%

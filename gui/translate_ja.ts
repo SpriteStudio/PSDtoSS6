0631173758885a087171388a03285b0f2cc2c2e6
WT-Yoshida <80303343+WT-Yoshida@users.noreply.github.com>
2021-06-02 19:38:01 +0900

    add translate file

--- gui/translate_ja.ts
+++ gui/translate_ja.ts
@@ -4,60 +4,267 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="262"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="332"/>
         <source>Register a txt file</source>
         <translation>txt ファイルを登録してください</translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="269"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="339"/>
         <source>Select an output folder</source>
         <translation>出力フォルダを選択してください</translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="358"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="438"/>
         <source>Convert Success!</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="362"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="442"/>
         <source>Error</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="422"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="502"/>
         <source>Output Directory</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="439"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="524"/>
         <source>select list File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="439"/>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="470"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="524"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="555"/>
         <source>text(*.txt)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="470"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="555"/>
         <source>save list File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="510"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="598"/>
         <source>select convert File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="510"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="598"/>
         <source>data(*.ss6-psdtoss6-info *.psd)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="C:/Users/nakata/Documents/git/PSDtoSS6/gui/mainwindow.cpp" line="618"/>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="706"/>
         <source>The current settings have been saved</source>
         <translation>現在の設定を保存しました</translation>
     </message>
-</context>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="742"/>
+        <source>Drop ss6-psdtoss6-info file and psd file from the script.</source>
+        <translation>スクリプトから出力したss6-psdtoss6-infoファイル、PSDファイルをドロップして追加してください.</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="744"/>
+        <source>Add file</source>
+        <translation>ファイル追加</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="745"/>
+        <source>Save file list</source>
+        <translation>リストの保存</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="746"/>
+        <source>Load file list</source>
+        <translation>リスト読み込み</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="747"/>
+        <source>Clear file list</source>
+        <translation>リストクリア</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="748"/>
+        <source>Select output folder</source>
+        <translation>出力フォルダ選択</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="749"/>
+        <source>Convert</source>
+        <translation>コンバート開始</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="750"/>
+        <source>Close</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="751"/>
+        <source>Save settings</source>
+        <translation>設定の保持</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="752"/>
+        <source>Help</source>
+        <translation>ヘルプ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="753"/>
+        <source>Preview</source>
+        <translation>プレビュー</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="755"/>
+        <source>Files to convert</source>
+        <translation>コンバートファイル</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="756"/>
+        <source>Output folder</source>
+        <translation>出力フォルダ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="757"/>
+        <source>Status</source>
+        <translation>ステータス</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="758"/>
+        <source>Log</source>
+        <translation>ログ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="760"/>
+        <source>Texture width</source>
+        <translation>テクスチャ幅</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="761"/>
+        <source>Texture height</source>
+        <translation>テクスチャ高さ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="762"/>
+        <source>Priority value</source>
+        <translation>優先度設定</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="763"/>
+        <source>Cell layout order</source>
+        <translation>セル配置順</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="764"/>
+        <source>Cell padding</source>
+        <translation>セル間余白</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="765"/>
+        <source>Cell margin</source>
+        <translation>セル内余白</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="766"/>
+        <source>Cell distance</source>
+        <translation>セル配置間隔</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="767"/>
+        <source>Cellmap margin</source>
+        <translation>セルマップ縁幅</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="768"/>
+        <source>Canvas frame</source>
+        <translation>基準枠サイズ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="770"/>
+        <source>Write ssae</source>
+        <translation>ssae出力</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="771"/>
+        <source>Write sspj</source>
+        <translation>sspj出力</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="772"/>
+        <source>Overwrite sspj,ssae</source>
+        <translation>sspj,ssae上書き</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="773"/>
+        <source>Add NULL parent</source>
+        <translation>親NULLパーツ追加</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="774"/>
+        <source>Apply pivot layer</source>
+        <translation>原点レイヤー適用</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="775"/>
+        <source>Apply root layer</source>
+        <translation>rootレイヤー適用</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/mainwindow.cpp" line="776"/>
+        <source>Read ssce</source>
+        <translation>ssce読み込み</translation>
+    </message>
+
+    <name>subwindow</name>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="305"/>
+        <source>Select/Deselect all</source>
+        <translation>全選択/全解除</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="306"/>
+        <source>More detail</source>
+        <translation>もっと詳しく</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="307"/>
+        <source>Close</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="309"/>
+        <source>Visible items</source>
+        <translation>可視化する項目</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="311"/>
+        <source>Texture width</source>
+        <translation>テクスチャ幅</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="312"/>
+        <source>Texture height</source>
+        <translation>テクスチャ高さ</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="313"/>
+        <source>Cell padding</source>
+        <translation>セル間余白</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="314"/>
+        <source>Cell margin</source>
+        <translation>セル内余白</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="315"/>
+        <source>Cell distance</source>
+        <translation>セル配置間隔</translation>
+    </message>
+    <message>
+        <location filename="C:/Users/KaiYoshida/Documents/Projects/PSDtoSS6/gui/subwindow.cpp" line="316"/>
+        <source>Cellmap margin</source>
+        <translation>セルマップ縁幅</translation>
+    </message>
+    </context>
 </TS>

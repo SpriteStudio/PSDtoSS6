e673c69e65163b047ac11871c382db7afe3ee2e0
WT-Yoshida <80303343+WT-Yoshida@users.noreply.github.com>
2021-06-02 19:38:53 +0900

    update main

--- gui/main.cpp
+++ gui/main.cpp
@@ -10,13 +10,20 @@ QTranslator translator;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
 
-	QTextCodec::setCodecForLocale(QTextCodec::codecForLocale());
+        QTextCodec::setCodecForLocale(QTextCodec::codecForLocale());
 	// 翻訳
-	translator.load("translate_ja");
-	a.installTranslator(&translator);
+        if(QLocale::system().name()=="ja_JP")
+        {
+            translator.load("translate_ja");
+        }
+        else
+        {
+            translator.load("translate_en");
+        }
+        a.installTranslator(&translator);
 
+    MainWindow w;
     w.setText_to_List(a.arguments());
     w.show();
 
